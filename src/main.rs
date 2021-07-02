769b72986f4972581cbf00031532cd76aed250cc
Reto Achermann <achreto@cs.ubc.ca>
2021-07-02 14:05:06 -0700

    adapting main to use the support library
    Signed-off-by: Reto Achermann <achreto@cs.ubc.ca>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
-//! Velosiraptor Compiler
+// Velosiraptor Lexer
+//
+//
+// MIT License
+//
+// Copyright (c) 2021 Systopia Lab, Computer Science, University of British Columbia
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
-// we are using nom as the parser
-extern crate nom;
+//! Velosiraptor Compiler
 
 // used for parsing
 use clap::{App, Arg};
@@ -9,8 +31,8 @@ use clap::{App, Arg};
 use simplelog::{Config, LevelFilter, SimpleLogger};
 
 // get the parser module
-// mod parser;
-// use parser::Parser;
+use libvrc::parser::Parser;
+
 
 fn parse_cmdline() -> clap::ArgMatches<'static> {
     App::new("vtrc")
@@ -68,18 +90,17 @@ fn main() {
     log::trace!("Tracing output enabled");
 
     // let's try to create a file parser
+    let ast = match Parser::parse_file(infile) {
+        Ok((ast, _)) => ast,
+        Err(x)       => {
+            log::error!("file parsing failed");
+            return;
+        }
+    };
+
+    println!("{}", ast);
     // let mut parser = Parser::from_file(infile.to_string()).expect("failed to construct the parser");
 
-    // // parse the file
-    // let err = parser.parse();
-    // if err.is_err() {
-    //     log::error!("Parsing failed.");
-    // }
-    // // resolve the import statements
-    // let err = parser.resolve_imports();
-    // if err.is_err() {
-    //     log::error!("Failed to resolve imports.");
-    // }
 
     // perform checks
 }
