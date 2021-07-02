30d3fcbff714a4dc5610401949bfcf1aa50be005
Reto Achermann <achreto@cs.ubc.ca>
2021-07-01 17:07:47 -0700

    lib/parser: adding module comment
    Signed-off-by: Reto Achermann <achreto@cs.ubc.ca>

--- lib/parser/mod.rs
+++ lib/parser/mod.rs
@@ -23,6 +23,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+//! Parser Module of the Velosiraptor Compiler
+
 use custom_error::custom_error;
 use std::rc::Rc;
 
