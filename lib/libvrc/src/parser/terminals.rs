cae784e4de5b4c91c8ee648e3d7a66855144ddfb
Reto Achermann <achreto@cs.ubc.ca>
2021-09-03 16:59:48 -0700

    lib/parser: adding ensures/requires/assert to keyword parser
    Signed-off-by: Reto Achermann <achreto@cs.ubc.ca>

--- lib/libvrc/src/parser/terminals.rs
+++ lib/libvrc/src/parser/terminals.rs
@@ -191,6 +191,9 @@ keywordparser!(pub kw_register, Keyword::Register);
 keywordparser!(pub kw_none, Keyword::None);
 keywordparser!(pub kw_fn, Keyword::Fn);
 keywordparser!(pub kw_size, Keyword::Size);
+keywordparser!(pub kw_requires, Keyword::Requires);
+keywordparser!(pub kw_ensures, Keyword::Ensures);
+keywordparser!(pub kw_assert, Keyword::Assert);
 
 /// parses a type expression
 ///
