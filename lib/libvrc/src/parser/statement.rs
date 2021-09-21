44bd39149a287d8a5ae41d965faa7c9b30c317c2
Reto Achermann <achreto@cs.ubc.ca>
2021-09-20 19:25:30 -0700

    lib/parser: improve statement parsing
    Signed-off-by: Reto Achermann <achreto@cs.ubc.ca>

--- lib/libvrc/src/parser/statement.rs
+++ lib/libvrc/src/parser/statement.rs
@@ -28,22 +28,49 @@
 // the used nom componets
 use nom::{
     branch::alt,
-    combinator::cut,
+    combinator::{cut, opt},
     multi::many1,
-    sequence::{delimited, pair, terminated},
+    sequence::{delimited, pair, preceded, terminated, tuple},
 };
 
 // lexer, parser terminals and ast
 use crate::ast::{Stmt, Type};
 use crate::error::IResult;
 use crate::parser::{
-    expression::{arith_expr, bool_expr},
+    expression::{arith_expr, bool_expr, expr},
     terminals::{
-        assign, colon, ident, kw_assert, kw_else, kw_if, kw_let, lbrace, lparen, rbrace, rparen,
-        semicolon, typeinfo,
+        assign, colon, ident, kw_assert, kw_else, kw_if, kw_let, kw_return, lbrace, lparen, rbrace,
+        rparen, semicolon, typeinfo,
     },
 };
 use crate::token::TokenStream;
+
+/// parses a return statement
+///
+/// The return statement produces the return value of a method
+///
+/// # Grammar
+///
+/// `STMT_RETURN := KW_RETURN EXPR;
+///
+/// # Example
+///
+/// `return 4;`
+/// `return state.foo;`
+///
+fn return_stmt(input: TokenStream) -> IResult<TokenStream, Stmt> {
+    // try to parse the `let` keyword, return error otherwise
+    let (i1, _) = kw_return(input.clone())?;
+
+    // parse the LHS identifier and type information `IDENT : TYPE =`
+    let (i2, exp) = cut(terminated(expr, semicolon))(i1)?;
+
+    // create the token stream covering the entire assert statement
+    let pos = input.expand_until(&i2);
+
+    // parsing successful, construct the token
+    Ok((i2, Stmt::Return { expr: exp, pos }))
+}
 
 /// parses a let statement
 ///
@@ -64,11 +91,8 @@ use crate::token::TokenStream;
 /// `let x : int = 123;`
 ///
 fn let_stmt(input: TokenStream) -> IResult<TokenStream, Stmt> {
-    // get the input position
-    let pos = input.input_sourcepos();
-
     // try to parse the `let` keyword, return error otherwise
-    let (i1, _) = kw_let(input)?;
+    let (i1, _) = kw_let(input.clone())?;
 
     // parse the LHS identifier and type information `IDENT : TYPE =`
     let (i2, (lhs, ti)) = cut(pair(ident, delimited(colon, typeinfo, assign)))(i1)?;
@@ -78,6 +102,9 @@ fn let_stmt(input: TokenStream) -> IResult<TokenStream, Stmt> {
         Type::Boolean => cut(terminated(bool_expr, semicolon))(i2),
         _ => cut(terminated(arith_expr, semicolon))(i2),
     }?;
+
+    // create the token stream covering the entire let statement
+    let pos = input.expand_until(&i3);
 
     // parsing successful, construct the token
     Ok((
@@ -109,48 +136,53 @@ fn let_stmt(input: TokenStream) -> IResult<TokenStream, Stmt> {
 ///
 /// `assert(in > 4);
 fn assert_stmt(input: TokenStream) -> IResult<TokenStream, Stmt> {
-    // get the input position
-    let pos = input.input_sourcepos();
-
     // try to parse the `let` keyword, return error otherwise
-    let (i1, _) = kw_assert(input)?;
+    let (i1, _) = kw_assert(input.clone())?;
 
     let (i2, expr) = cut(terminated(delimited(lparen, bool_expr, rparen), semicolon))(i1)?;
+
+    // create the token stream covering the entire assert statement
+    let pos = input.expand_until(&i2);
 
     Ok((i2, Stmt::Assert { expr, pos }))
 }
 
-/// parser the if/else statement
+/// parses an if/else statement
+///
+/// The if/else statement provides a conditional branching
+///
+/// # Grammar
+///
+/// STMT_IFELSE := KW_IF EXPR { STMT } [KW_ELSE { EXPR }]
+///
+/// # Results
+///
+/// # Examples
+///
+/// `if a < 0 { foo; } else { bar; }`
 fn if_else_stmt(input: TokenStream) -> IResult<TokenStream, Stmt> {
-    let pos = input.input_sourcepos();
     // try to parse the `if` keyword, return error otherwise
-    let (input, _tok) = kw_if(input)?;
+    let (i1, _tok) = kw_if(input.clone())?;
 
-    let (input, (cond, then)) = pair(bool_expr, delimited(lbrace, stmt, rbrace))(input)?;
+    // parses the block of statement
 
-    match kw_else(input.clone()) {
-        Ok((input, _)) => match delimited(lbrace, stmt, rbrace)(input) {
-            Ok((rem, stmt)) => Ok((
-                rem,
-                Stmt::IfElse {
-                    cond,
-                    then: Box::new(then),
-                    other: Some(Box::new(stmt)),
-                    pos,
-                },
-            )),
-            Err(e) => Err(e),
+    let then_block = cut(delimited(lbrace, many1(stmt), rbrace));
+    let else_block = preceded(kw_else, cut(delimited(lbrace, many1(stmt), rbrace)));
+
+    let (i2, (cond, then, other)) = tuple((bool_expr, then_block, opt(else_block)))(i1)?;
+
+    // create the token stream covering the entire method def
+    let pos = input.expand_until(&i2);
+
+    Ok((
+        i2,
+        Stmt::IfElse {
+            cond,
+            then,
+            other: other.unwrap_or(Vec::new()),
+            pos,
         },
-        Err(_) => Ok((
-            input,
-            Stmt::IfElse {
-                cond,
-                then: Box::new(then),
-                other: None,
-                pos,
-            },
-        )),
-    }
+    ))
 }
 
 /// Parses a statement
@@ -164,9 +196,7 @@ fn if_else_stmt(input: TokenStream) -> IResult<TokenStream, Stmt> {
 ///
 ///z
 pub fn stmt(input: TokenStream) -> IResult<TokenStream, Stmt> {
-    let pos = input.input_sourcepos();
-    let (i, stmts) = many1(alt((if_else_stmt, let_stmt, assert_stmt)))(input)?;
-    Ok((i, Stmt::Block { stmts, pos }))
+    alt((if_else_stmt, let_stmt, assert_stmt, return_stmt))(input)
 }
 
 #[test]
