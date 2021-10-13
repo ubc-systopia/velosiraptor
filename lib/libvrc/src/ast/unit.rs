9823c227b80b69f67c6341a50ad99cd6083fa39e
Reto Achermann <achreto@cs.ubc.ca>
2021-10-13 10:27:09 -0700

    lib/ast: more checks for defined params
    Signed-off-by: Reto Achermann <achreto@cs.ubc.ca>

--- lib/libvrc/src/ast/unit.rs
+++ lib/libvrc/src/ast/unit.rs
@@ -86,7 +86,14 @@ impl AstNode for Unit {
         // set the current context
         st.create_context(String::from(self.name()));
 
-        // add th emethods to it
+        // adding the module paramters
+        for p in &self.params {
+            if !st.insert(p.to_symbol()) {
+                res.inc_err(1);
+            }
+        }
+
+        // add the methods to it
         for m in &self.methods {
             if !st.insert(m.to_symbol()) {
                 res.inc_err(1);
