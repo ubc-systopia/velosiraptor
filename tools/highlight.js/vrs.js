97cf2084984bdc27da6d579c5c78c0590148c3d2
Reto Achermann <achreto@gmail.com>
2021-11-24 12:49:23 -0800

    adding highlight.js syntax for vrs
    Signed-off-by: Reto Achermann <achreto@gmail.com>

---
+++ tools/highlight.js/vrs.js
@@ -0,0 +1,203 @@
+/*
+Language: Rust
+Author: Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
+Contributors: Roman Shmatov <romanshmatov@gmail.com>, Kasper Andersen <kma_untrusted@protonmail.com>
+Website: https://www.rust-lang.org
+Category: common, system
+*/
+
+/** @type LanguageFn */
+export default function(hljs) {
+  const regex = hljs.regex;
+  const FUNCTION_INVOKE = {
+    className: "title.function.invoke",
+    relevance: 0,
+    begin: regex.concat(
+      /\b/,
+      /(?!let\b)/,
+      hljs.IDENT_RE,
+      regex.lookahead(/\s*\(/))
+  };
+  const NUMBER_SUFFIX = '([ui](8|16|32|64|128|size)|f(32|64))\?';
+  const KEYWORDS = [
+    "bool",
+    "const",
+    "else",
+    "ensures",
+    "exists",
+    "fn",
+    "for",
+    "forall",
+    "if",
+    "import",
+    "in",
+    "Layout",
+    "let",
+    "map",
+    "Memory",
+    "MMIO",
+    "ReadAction",
+    "Register",
+    "requires",
+    "return",
+    "unit",
+    "WriteAction",
+  ];
+  const LITERALS = [
+    "true",
+    "false",
+    "None",
+  ];
+  const BUILTINS = [
+    "state",
+    "interface",
+    "assert",
+    "staticmap"
+  ];
+  const TYPES = [
+    "int",
+    "addr",
+    "bool",
+    "size"
+  ];
+  return {
+    name: 'VelosiRaptor Specification Language',
+    aliases: [ 'vrs' ],
+    keywords: {
+      $pattern: hljs.IDENT_RE + '!?',
+      type: TYPES,
+      keyword: KEYWORDS,
+      literal: LITERALS,
+      built_in: BUILTINS
+    },
+    illegal: '</',
+    contains: [
+      hljs.C_LINE_COMMENT_MODE,
+      hljs.COMMENT('/\\*', '\\*/', {
+        contains: [ 'self' ]
+      }),
+      hljs.inherit(hljs.QUOTE_STRING_MODE, {
+        begin: /b?"/,
+        illegal: null
+      }),
+      {
+        className: 'string',
+        variants: [
+          {
+            begin: /b?r(#*)"(.|\n)*?"\1(?!#)/
+          },
+          {
+            begin: /b?'\\?(x\w{2}|u\w{4}|U\w{8}|.)'/
+          }
+        ]
+      },
+      {
+        className: 'symbol',
+        begin: /'[a-zA-Z_][a-zA-Z0-9_]*/
+      },
+      {
+        className: 'number',
+        variants: [
+          {
+            begin: '\\b0b([01_]+)' + NUMBER_SUFFIX
+          },
+          {
+            begin: '\\b0o([0-7_]+)' + NUMBER_SUFFIX
+          },
+          {
+            begin: '\\b0x([A-Fa-f0-9_]+)' + NUMBER_SUFFIX
+          },
+          {
+            begin: '\\b(\\d[\\d_]*(\\.[0-9_]+)?([eE][+-]?[0-9_]+)?)' +
+                   NUMBER_SUFFIX
+          }
+        ],
+        relevance: 0
+      },
+      {
+        begin: [
+          /fn/,
+          /\s+/,
+          hljs.UNDERSCORE_IDENT_RE
+        ],
+        className: {
+          1: "keyword",
+          3: "title.function"
+        }
+      },
+      {
+        className: 'meta',
+        begin: '#!?\\[',
+        end: '\\]',
+        contains: [
+          {
+            className: 'string',
+            begin: /"/,
+            end: /"/
+          }
+        ]
+      },
+      {
+        begin: [
+          /let/, /\s+/,
+          /(?:mut\s+)?/,
+          hljs.UNDERSCORE_IDENT_RE
+        ],
+        className: {
+          1: "keyword",
+          3: "keyword",
+          4: "variable"
+        }
+      },
+      // must come before impl/for rule later
+      {
+        begin: [
+          /for/,
+          /\s+/,
+          hljs.UNDERSCORE_IDENT_RE,
+          /\s+/,
+          /in/
+        ],
+        className: {
+          1: "keyword",
+          3: "variable",
+          5: "keyword"
+        }
+      },
+      {
+        begin: [
+          /type/,
+          /\s+/,
+          hljs.UNDERSCORE_IDENT_RE
+        ],
+        className: {
+          1: "keyword",
+          3: "title.class"
+        }
+      },
+      {
+        begin: [
+          /(?:trait|enum|struct|union|impl|for)/,
+          /\s+/,
+          hljs.UNDERSCORE_IDENT_RE
+        ],
+        className: {
+          1: "keyword",
+          3: "title.class"
+        }
+      },
+      {
+        begin: hljs.IDENT_RE + '::',
+        keywords: {
+          keyword: "Self",
+          built_in: BUILTINS
+        }
+      },
+      {
+        className: "punctuation",
+        begin: '->'
+      },
+      FUNCTION_INVOKE
+    ]
+  };
+}
