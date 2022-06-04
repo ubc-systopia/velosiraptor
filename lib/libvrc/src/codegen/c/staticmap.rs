20aa0096d4d64920b944b285e02eaffac50e2484
Reto Achermann <achreto@cs.ubc.ca>
2022-06-03 17:21:54 -0700

    wip commit
    Signed-off-by: Reto Achermann <achreto@cs.ubc.ca>

--- lib/libvrc/src/codegen/c/staticmap.rs
+++ lib/libvrc/src/codegen/c/staticmap.rs
@@ -23,7 +23,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-//! Segment Generation (C)
+//! StaticMap Generation (C)
 
 use std::collections::{HashMap, HashSet};
 use std::path::Path;
