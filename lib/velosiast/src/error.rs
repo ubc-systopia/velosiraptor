3e91117e844fc736ef79e7503b8734dcfcc163dd
Reto Achermann <achreto@cs.ubc.ca>
2022-10-15 14:48:03 -0700

    lib/velosiast: start with the implementation for the ast conversion
    Signed-off-by: Reto Achermann <achreto@cs.ubc.ca>

---
+++ lib/velosiast/src/error.rs
@@ -0,0 +1,492 @@
+// Velosilexer Error
+//
+//
+// MIT License
+//
+// Copyright (c) 2021, 2022 Systopia Lab, Computer Science, University of British Columbia
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+//! # Parser Errors for the VelosiParser
+
+//
+use std::fmt::{Display, Formatter, Result};
+use std::ops::Add;
+use std::rc::Rc;
+
+// external dependencies
+use colored::*;
+
+use crate::VelosiTokenStream;
+
+fn print_location_line(f: &mut Formatter<'_>, warn: bool, tokstream: &VelosiTokenStream) -> Result {
+    let blue = |s: &str| s.bold().blue();
+    let pipe = blue("|");
+
+    // location information
+    let location = tokstream.span();
+    writeln!(f, "     {} {}", blue("-->"), location.loc())?;
+    writeln!(f, "      {}", pipe)
+}
+
+fn print_location_context(
+    f: &mut Formatter<'_>,
+    warn: bool,
+    tokstream: &VelosiTokenStream,
+) -> Result {
+    let blue = |s: &str| s.bold().blue();
+    let highlight = if warn {
+        |s: &str| s.bold().bright_yellow()
+    } else {
+        |s: &str| s.bold().bright_red()
+    };
+
+    let pipe = blue("|");
+
+    let location = tokstream.span();
+
+    let linenum = location.line().to_string();
+
+    let linectxt = location.srcline();
+
+    if linectxt.len() <= 100 {
+        // calculate the indentation
+        let col = location.column();
+        let indent = (0..col - 1).map(|_| " ").collect::<String>();
+
+        // get the underline
+        let ulen = std::cmp::min(location.len(), linectxt.len());
+        let underline = (0..ulen).map(|_| "^").collect::<String>();
+
+        // the line context with highligted part that is wrong
+        writeln!(f, " {:>4} {}  {}", blue(&linenum), pipe, linectxt)?;
+        write!(f, "      {}  {}{}", pipe, indent, highlight(&underline))
+    } else {
+        // we're longer than 100 characters, so truncate the output
+        let col = location.column() as usize;
+
+        let printrange = if col > 50 {
+            let end = std::cmp::min(col + 50, linectxt.len());
+            // just print stuff around colum +/- 50
+            col - 50..end
+        } else {
+            // take verything from the beginning
+            0..100
+        };
+
+        let indent = (printrange.start..col - 1).map(|_| " ").collect::<String>();
+
+        let ulen = std::cmp::min(location.len(), printrange.end - printrange.start);
+        let underline = (0..ulen).map(|_| "^").collect::<String>();
+
+        // the line context with highligted part that is wrong
+        writeln!(
+            f,
+            " {:>4} {}         ... {}... ",
+            blue(&linenum),
+            pipe,
+            &linectxt[printrange]
+        )?;
+        write!(
+            f,
+            "      {}             {}{}",
+            pipe,
+            indent,
+            highlight(&underline),
+        )
+    }
+}
+
+fn print_location(f: &mut Formatter<'_>, warn: bool, tokstream: &VelosiTokenStream) -> Result {
+    print_location_line(f, warn, tokstream)?;
+    print_location_context(f, warn, tokstream)
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Custom Error with Builder
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub(crate) struct VelosiAstErrBuilder {
+    error: bool,
+    message: String,
+    hint: Option<String>,
+    tokstream: Option<VelosiTokenStream>,
+}
+
+impl VelosiAstErrBuilder {
+    pub fn warn(message: String) -> Self {
+        Self {
+            error: false,
+            message,
+            hint: None,
+            tokstream: None,
+        }
+    }
+
+    pub fn err(message: String) -> Self {
+        Self {
+            error: true,
+            message,
+            hint: None,
+            tokstream: None,
+        }
+    }
+
+    /// Adds a hint to the error message
+    pub fn add_hint(&mut self, hint: String) -> &mut Self {
+        self.hint = Some(hint);
+        self
+    }
+
+    /// Adds a location to the error information
+    pub fn add_location(&mut self, tokstream: VelosiTokenStream) -> &mut Self {
+        self.tokstream = Some(tokstream);
+        self
+    }
+
+    pub fn build(&mut self) -> VelosiAstErr {
+        VelosiAstErr::Custom(VelosiAstErrCustom {
+            message: self.message.clone(),
+            error: self.error,
+            hint: self.hint.take(),
+            tokstream: self.tokstream.take().unwrap_or_default(),
+        })
+    }
+}
+
+/// Defines a Lexer Error
+#[derive(PartialEq, Eq, Debug)]
+pub struct VelosiAstErrCustom {
+    /// error message
+    message: String,
+    /// whether this is an error or a warning
+    error: bool,
+    /// Hint
+    hint: Option<String>,
+    /// the location where the error happened
+    tokstream: VelosiTokenStream,
+}
+
+/// Implementation of [Display] for [VelosiAstErrCustom]
+impl Display for VelosiAstErrCustom {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        // closure for coloring
+        let red = |s: &str| s.bright_red().bold();
+        let blue = |s: &str| s.bold().blue();
+
+        // the error message
+        writeln!(f, "{}{} {}", red("error"), ":".bold(), self.message.bold())?;
+
+        // the location, if it is set
+        if !self.tokstream.span().is_default() {
+            print_location(f, self.error, &self.tokstream)?;
+        }
+
+        // apply the hint if needed
+        match &self.hint {
+            Some(h) => writeln!(f, " {}", red(h.as_str())),
+            None => writeln!(f),
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Double Defined Symbols
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct VelosiAstErrDoubleDef {
+    /// the name of the double defined variable
+    name: Rc<String>,
+    /// the location of the first definition
+    first: VelosiTokenStream,
+    /// the location of the second definition
+    second: VelosiTokenStream,
+}
+
+impl VelosiAstErrDoubleDef {
+    pub fn new(name: Rc<String>, first: VelosiTokenStream, second: VelosiTokenStream) -> Self {
+        Self {
+            name,
+            first,
+            second,
+        }
+    }
+}
+
+impl From<VelosiAstErrDoubleDef> for VelosiAstErr {
+    fn from(err: VelosiAstErrDoubleDef) -> Self {
+        VelosiAstErr::DoubleDef(err)
+    }
+}
+
+impl Display for VelosiAstErrDoubleDef {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        // closure for coloring
+        let red = |s: &str| s.bright_red().bold();
+        let blue = |s: &str| s.bold().blue();
+
+        // the error message
+        let msg = format!("duplicate definitions with name `{}`", self.name);
+        writeln!(f, "{}{} {}", red("error"), ":".bold(), msg.bold())?;
+
+        let pipe = blue("|");
+
+        // location information
+        writeln!(f, "     {} {}", blue("-->"), self.second.loc())?;
+        writeln!(f, "      {}", pipe)?;
+
+        print_location_context(f, false, &self.first)?;
+        let m = format!("previous definition of `{}` was here", self.name);
+        writeln!(f, " {}", red(m.as_str()))?;
+        writeln!(f, "   {}", blue("..."))?;
+        writeln!(f, "      {}", pipe)?;
+        print_location_context(f, false, &self.second)?;
+        writeln!(f, " {}", red("duplicate definition"))
+
+        //         error[E0201]: duplicate definitions with name `new`:
+        //    --> lib/velosiast/src/error.rs:216:5
+        //     |
+        // 209 |     pub fn new(name: Rc<String>, first: VelosiTokenStream, second: VelosiTokenStream) -> Self {
+        //     |     ----------------------------------------------------------------------------------------- previous definition of `new` here
+        // ...
+        // 216 |     pub fn new() {}
+        //     |     ^^^^^^^^^^^^ duplicate definition
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Undefined symbols
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct VelosiAstErrUndef {
+    /// the name of the undefined identifier
+    name: Rc<String>,
+    /// the location of the undefined identifier occurrance
+    loc: VelosiTokenStream,
+    /// if set, the location of the symbol of different type
+    other: Option<VelosiTokenStream>,
+}
+
+impl VelosiAstErrUndef {
+    pub fn new(name: Rc<String>, loc: VelosiTokenStream) -> Self {
+        Self {
+            name,
+            loc,
+            other: None,
+        }
+    }
+
+    pub fn with_other(name: Rc<String>, loc: VelosiTokenStream, other: VelosiTokenStream) -> Self {
+        Self {
+            name,
+            loc,
+            other: Some(other),
+        }
+    }
+}
+
+impl From<VelosiAstErrUndef> for VelosiAstErr {
+    fn from(err: VelosiAstErrUndef) -> Self {
+        VelosiAstErr::Undefined(err)
+    }
+}
+
+impl Display for VelosiAstErrUndef {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        // closure for coloring
+        let red = |s: &str| s.bright_red().bold();
+        let blue = |s: &str| s.bold().blue();
+
+        // the error message
+        let m = if self.other.is_some() {
+            format!(
+                "symbol named `{}` was defined with a different kind",
+                self.name
+            )
+        } else {
+            format!("no symbol named `{}` found in the current scope", self.name)
+        };
+
+        writeln!(f, "{}{} {}", red("error"), ":".bold(), m.bold())?;
+
+        // the location, if it is set
+        if !self.loc.span().is_default() {
+            print_location(f, false, &self.loc)?;
+        }
+
+        if let Some(other) = &self.other {
+            let m = "this is the symbol with the wrong kind";
+            writeln!(f, " {}", red(m))?;
+            print_location(f, false, other)
+        } else {
+            writeln!(f, " {}", red("not found in this scope"))
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Ast Errors
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Defines a Lexer Error
+#[derive(PartialEq, Eq, Debug)]
+pub enum VelosiAstErr {
+    DoubleDef(VelosiAstErrDoubleDef),
+    Undefined(VelosiAstErrUndef),
+    Custom(VelosiAstErrCustom),
+}
+
+impl VelosiAstErr {
+    pub fn is_warn(&self) -> bool {
+        match self {
+            VelosiAstErr::Custom(err) => !err.error,
+            _ => false,
+        }
+    }
+}
+
+/// Implementation of [Display] for [VelosiAstErr]
+impl Display for VelosiAstErr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            VelosiAstErr::DoubleDef(e) => e.fmt(f),
+            VelosiAstErr::Custom(e) => e.fmt(f),
+            VelosiAstErr::Undefined(e) => e.fmt(f),
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Ast Issues
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct VelosiAstIssues {
+    errors: Vec<VelosiAstErr>,
+    num_errors: usize,
+    num_warnings: usize,
+}
+
+impl VelosiAstIssues {
+    pub fn new() -> Self {
+        Self {
+            errors: Vec::new(),
+            num_errors: 0,
+            num_warnings: 0,
+        }
+    }
+
+    pub fn has_errors(&self) -> bool {
+        self.num_errors > 0
+    }
+
+    pub fn has_warnings(&self) -> bool {
+        self.num_warnings > 0
+    }
+
+    pub fn is_ok(&self) -> bool {
+        self.num_errors == 0 && self.num_warnings == 0
+    }
+
+    pub fn push(&mut self, issue: VelosiAstErr) {
+        if issue.is_warn() {
+            self.num_warnings += 1;
+        } else {
+            self.num_errors += 1;
+        }
+        self.errors.push(issue);
+    }
+
+    pub fn merge(&mut self, other: VelosiAstIssues) {
+        self.num_errors += other.num_errors;
+        self.num_warnings += other.num_warnings;
+        self.errors.extend(other.errors);
+    }
+}
+
+impl Default for VelosiAstIssues {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Add for VelosiAstIssues {
+    type Output = Self;
+
+    fn add(mut self, other: Self) -> Self {
+        self.errors.extend(other.errors);
+        Self {
+            errors: self.errors,
+            num_errors: self.num_errors + other.num_errors,
+            num_warnings: self.num_warnings + other.num_warnings,
+        }
+    }
+}
+
+impl From<VelosiAstErr> for VelosiAstIssues {
+    fn from(err: VelosiAstErr) -> Self {
+        let mut issues = Self::new();
+        issues.push(err);
+        issues
+    }
+}
+
+impl From<VelosiAstErrUndef> for VelosiAstIssues {
+    fn from(err: VelosiAstErrUndef) -> Self {
+        let mut issues = Self::new();
+        issues.push(VelosiAstErr::Undefined(err));
+        issues
+    }
+}
+
+/// Implementation of [Display] for [VelosiAstIssues]
+impl Display for VelosiAstIssues {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        for e in &self.errors {
+            Display::fmt(e, f)?;
+        }
+
+        let red = |s: &str| s.bright_red().bold();
+        let yellow = |s: &str| s.bright_yellow().bold();
+        if self.num_warnings > 0 {
+            writeln!(
+                f,
+                "{}{} generated {} warnings",
+                yellow("warning"),
+                ":".bold(),
+                self.num_warnings
+            )?;
+        }
+
+        if self.num_errors > 0 {
+            writeln!(
+                f,
+                "{}{} could not compile due to {} previous errors; {} warnigns emitted",
+                red("error"),
+                ":".bold(),
+                self.num_errors,
+                self.num_warnings
+            )?;
+        }
+
+        Ok(())
+    }
+}
---
+++ lib/velosiast/src/symboltable.rs
@@ -0,0 +1,248 @@
+// VelosiAst -- a ast for the Velosiraptor Language
+//
+//
+// MIT License
+//
+// Copyright (c) 2021 Systopia Lab, Computer Science, University of British Columbia
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+//! A Symboltable Implementation
+//!
+//! # General Structure
+//!
+
+// the used std library functionality
+use std::collections::HashMap;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::rc::Rc;
+
+// the used library internal functionality
+use crate::ast::{VelosiAstNode, VelosiAstType};
+use crate::error::{VelosiAstErr, VelosiAstErrDoubleDef};
+use crate::VelosiTokenStream;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Symbol Definition
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Represents a symbol in the AST
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct Symbol {
+    /// the name of the symbol, its identifier
+    pub name: Rc<String>,
+    /// the type of the symbol, `int|bool|...`
+    pub typeinfo: VelosiAstType,
+    /// the astnode of this symbol
+    pub ast_node: VelosiAstNode,
+}
+
+/// Implementation of [Symbol]
+impl Symbol {
+    /// creates a new symbol from the given context and name, type
+    pub fn new(name: Rc<String>, typeinfo: VelosiAstType, ast_node: VelosiAstNode) -> Self {
+        Symbol {
+            name,
+            typeinfo,
+            ast_node,
+        }
+    }
+
+    /// obtains the location of the symbol
+    pub fn loc(&self) -> &VelosiTokenStream {
+        self.ast_node.loc()
+    }
+}
+
+/// Implementation of the [Display] trait for [Symbol]
+///
+/// We display it as a table form kind|type|name
+impl Display for Symbol {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        match self.ast_node {
+            VelosiAstNode::Unit(_) => write!(f, "unit|{:?}|{}", self.typeinfo, self.name),
+            VelosiAstNode::Const(_) => write!(f, "const|{:?}|{}", self.typeinfo, self.name),
+            VelosiAstNode::Method => write!(f, "method|{:?}|{}", self.typeinfo, self.name),
+            VelosiAstNode::Param(_) => write!(f, "param|{:?}|{}", self.typeinfo, self.name),
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Symbol Tabel Context
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Represents a symbol table context
+///
+/// The symbol table context is contains the symbols that are introduced by the current
+/// scope, e.g., unit or method contxt.
+#[derive(PartialEq, Eq, Clone, Debug)]
+struct SymbolTableContext {
+    /// the symbols of the table, we use a vector
+    syms: HashMap<String, Symbol>,
+    /// the currenct context
+    ctxt: String,
+}
+
+impl SymbolTableContext {
+    /// tries to insert a new symbol into the context
+    fn insert(&mut self, sym: Symbol) -> Result<(), VelosiAstErr> {
+        match self.syms.get(sym.name.as_str()) {
+            None => {
+                self.syms.insert(sym.name.to_string(), sym);
+                Ok(())
+            }
+            Some(x) => Err(VelosiAstErrDoubleDef::new(
+                sym.name.clone(),
+                x.ast_node.loc().clone(),
+                sym.ast_node.loc().clone(),
+            )
+            .into()),
+        }
+    }
+
+    /// removes an entry from the context
+    fn remove(&mut self, name: &str) -> Option<Symbol> {
+        self.syms.remove(name)
+    }
+
+    /// lookup a symbol by its name
+    fn lookup(&self, name: &str) -> Option<&Symbol> {
+        self.syms.get(name)
+    }
+
+    /// checks if the context contains a given key
+    fn contains(&self, name: &str) -> bool {
+        self.lookup(name).is_some()
+    }
+
+    /// the number of symbols in the context
+    fn count(&self) -> usize {
+        self.syms.len()
+    }
+}
+
+/// Implementation of the [fmt::Display] trait for [SymbolTableContext]
+impl Display for SymbolTableContext {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        writeln!(f, "Context: {}", self.ctxt)?;
+        writeln!(f, "Kind         Type       Name                    Loc")?;
+        for c in self.syms.values() {
+            writeln!(f, "{}", c)?;
+        }
+        Ok(())
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Symbol Tabel
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Represents a symbol table
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct SymbolTable {
+    /// A vector of symbol table contexts
+    syms: Vec<SymbolTableContext>,
+}
+
+/// Implementation of [SymbolTable]
+impl SymbolTable {
+    /// creates a new empty symbol table
+    pub fn new() -> Self {
+        let mut st = SymbolTable { syms: Vec::new() };
+        st.create_context(String::from("global"));
+        st
+    }
+
+    /// creates a new context within the symbol table
+    pub fn create_context(&mut self, context: String) {
+        self.syms.push(SymbolTableContext {
+            syms: HashMap::new(),
+            ctxt: context,
+        })
+    }
+
+    /// drops the current active context
+    pub fn drop_context(&mut self) {
+        if self.syms.len() > 1 {
+            self.syms.pop();
+        }
+    }
+
+    /// tries to insert the symbol into the current context
+    pub fn insert(&mut self, sym: Symbol) -> Result<(), VelosiAstErr> {
+        match self.lookup(&sym.name) {
+            None => {
+                let ctxt = self.syms.last_mut().unwrap();
+                ctxt.insert(sym)
+            }
+            Some(x) => Err(VelosiAstErrDoubleDef::new(
+                sym.name.clone(),
+                x.ast_node.loc().clone(),
+                sym.ast_node.loc().clone(),
+            )
+            .into()),
+        }
+    }
+
+    /// checks if there is a corresponding entry in the table
+    pub fn contains(&self, sym: &str) -> bool {
+        self.syms.iter().any(|x| x.contains(sym))
+    }
+
+    /// tries to lookup a symbol by the name in all contexts
+    pub fn lookup(&self, name: &str) -> Option<&Symbol> {
+        for ctxt in &self.syms {
+            let sym = ctxt.lookup(name);
+            if sym.is_some() {
+                return sym;
+            }
+        }
+        None
+    }
+
+    /// removes a symbol from the current context
+    pub fn remove(&mut self, sym: &str) -> Option<Symbol> {
+        let ctxt = self.syms.last_mut().unwrap();
+        ctxt.remove(sym)
+    }
+
+    /// the number of symbols in the context
+    pub fn count(&self) -> usize {
+        self.syms.iter().fold(0, |v, c| v + c.count())
+    }
+}
+
+/// Implementation of the [fmt::Display] trait for [SymbolTable]
+impl Display for SymbolTable {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        writeln!(f, "SYMBOL TABLE\n")?;
+        for c in &self.syms {
+            writeln!(f, "{}", c)?;
+        }
+        Ok(())
+    }
+}
+
+/// the default implementation
+impl Default for SymbolTable {
+    fn default() -> Self {
+        Self::new()
+    }
+}
