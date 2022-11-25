7ed201739b98ac6c2ecffe12be3ea0fa65e8e1e4
Reto Achermann <achreto@cs.ubc.ca>
2022-11-24 16:33:59 -0800

    lib/velosiast: disabling print outputs
    Signed-off-by: Reto Achermann <achreto@cs.ubc.ca>

--- lib/velosiast/src/ast/method.rs
+++ lib/velosiast/src/ast/method.rs
@@ -228,7 +228,6 @@ impl VelosiAstMethod {
 
             // for the flags we need to add the flags to the symbol table
             if param.ptype.is_flags() {
-                println!("param is flags!");
                 let flags = if let Some(f) = st.lookup("flags") {
                     if let VelosiAstNode::Flags(flags) = &f.ast_node {
                         // clone the RC
