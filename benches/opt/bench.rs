44fcf9d33b8130c62f7eded1cd4eec8a940aa3f9
Reto Achermann <achreto@cs.ubc.ca>
2024-02-14 09:11:49 -0800

    benches: adding optimization benchmark
    Signed-off-by: Reto Achermann <achreto@cs.ubc.ca>

---
+++ benches/opt/bench.rs
@@ -0,0 +1,149 @@
+pub const NUM_WORKERS: usize = 14;
+pub const ITERATIONS: usize = 100;
+
+pub struct Stats {
+    pub min: u128,
+    pub med: u128,
+    pub avg: u128,
+    pub max: u128,
+    pub var: u128,
+    pub std: u128,
+    pub num: usize,
+}
+
+impl From<&[u128]> for Stats {
+    fn from(stats: &[u128]) -> Self {
+        let mut data = stats.to_vec();
+
+        if data.is_empty() {
+            return Self {
+                min: 0,
+                med: 0,
+                avg: 0,
+                max: 0,
+                var: 0,
+                std: 0,
+                num: 0,
+            };
+        }
+
+        data.sort();
+
+        let sum = data.iter().sum::<u128>() as u128;
+        let num = data.len();
+        let avg = sum / num as u128;
+
+        let var = data.iter().map(|x| (x - avg) * (x - avg)).sum::<u128>() as u128 / num as u128;
+        let std = (var as f64).sqrt() as u128;
+        Self {
+            min: *data.first().unwrap(),
+            med: data[num / 2],
+            avg,
+            max: *data.last().unwrap(),
+            var,
+            std,
+            num,
+        }
+    }
+}
+
+impl From<&[usize]> for Stats {
+    fn from(stats: &[usize]) -> Self {
+        let data: Vec<u128> = stats.iter().map(|x| *x as u128).collect();
+        Stats::from(data.as_slice())
+    }
+}
+
+pub struct BenchResults {
+    pub tag: String,
+    pub num_segments: usize,
+    pub num_staticmaps: usize,
+    pub num_enums: usize,
+    pub num_units: usize,
+    pub num_fields: usize,
+    pub num_slices: usize,
+    pub n_queries: Vec<usize>,
+    pub n_cached: Vec<usize>,
+    pub n_programs: u128,
+    pub n_programs_max: Option<u128>,
+}
+
+impl BenchResults {
+    pub fn new(tag: String) -> Self {
+        Self {
+            tag,
+            num_segments: 0,
+            num_staticmaps: 0,
+            num_enums: 0,
+            num_units: 0,
+            num_fields: 0,
+            num_slices: 0,
+            n_programs: 0,
+            n_programs_max: None,
+            n_queries: Vec::new(),
+            n_cached: Vec::new(),
+        }
+    }
+
+    pub fn merge(&mut self, other: &Self) {
+        assert_eq!(self.tag, other.tag);
+        self.n_cached.extend(other.n_cached.iter());
+        self.n_queries.extend(other.n_queries.iter());
+        if self.num_segments == 0 {
+            self.num_segments = other.num_segments;
+        } else {
+            assert_eq!(self.num_segments, other.num_segments);
+        }
+        if self.num_staticmaps == 0 {
+            self.num_staticmaps = other.num_staticmaps;
+        } else {
+            assert_eq!(self.num_staticmaps, other.num_staticmaps);
+        }
+        if self.num_enums == 0 {
+            self.num_enums = other.num_enums;
+        } else {
+            assert_eq!(self.num_enums, other.num_enums);
+        }
+        if self.num_units == 0 {
+            self.num_units = other.num_units;
+        } else {
+            assert_eq!(self.num_units, other.num_units);
+        }
+        if self.num_fields == 0 {
+            self.num_fields = other.num_fields;
+        } else {
+            assert_eq!(self.num_fields, other.num_fields);
+        }
+        if self.num_slices == 0 {
+            self.num_slices = other.num_slices;
+        } else {
+            assert_eq!(self.num_slices, other.num_slices);
+        }
+        if self.n_programs == 0 {
+            self.n_programs = other.n_programs;
+        } else {
+            assert_eq!(self.n_programs, other.n_programs);
+        }
+        if self.n_programs_max.is_none() {
+            self.n_programs_max = other.n_programs_max;
+        } else {
+            assert_eq!(self.n_programs_max, other.n_programs_max);
+        }
+    }
+}
+
+impl std::fmt::Display for BenchResults {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{:<30} {:2}U+{:2}F+{:2}S   search space: {}",
+            self.tag,
+            self.num_units,
+            self.num_fields,
+            self.num_slices,
+            self.n_programs_max.unwrap_or_default()
+        )?;
+
+        Ok(())
+    }
+}
---
+++ benches/opt/main.rs
@@ -0,0 +1,216 @@
+use std::collections::HashMap;
+use std::path::PathBuf;
+use std::rc::Rc;
+use std::time::Instant;
+
+use velosiast::{
+    AstResult, VelosiAst, VelosiAstField, VelosiAstUnit, VelosiAstUnitEnum, VelosiAstUnitSegment,
+};
+use velosisynth::{SynthOpts, Z3SynthEnum, Z3SynthSegment, Z3WorkerPool};
+
+const SPECS: [(&str, &str); 10] = [
+    ("examples/simple_translation_table.vrs", "Simple Page Table"),
+    ("examples/x86_32_pagetable.vrs", "x86\\_32 Page Table"),
+    ("examples/x86_64_pagetable.vrs", "x86\\_64 Page Table"),
+    ("examples/xeon_phi_smpt.vrs", "Xeon Phi SMPT"),
+    ("examples/simple_segment.vrs", "Simple Segment"),
+    ("examples/variable_segment.vrs", "Variable Segment"),
+    ("examples/medium.vrs", "Medium Segment"),
+    ("examples/x86_segmentation.vrs", "x86 Segmentation"),
+    ("examples/assoc_segment.vrs", "Assoc Segment"),
+    ("examples/r4700_fixed_page_size.vrs", "R4700 TLB"),
+];
+
+const OPTS: [(&str, (bool, bool, bool, bool, bool)); 5] = [
+    //               iface  state  expr   cache   tree
+    ("No Opts", (true, true, true, true, true)),
+    ("+ IfaceOpt", (false, true, true, true, true)),
+    ("+ StateOpt", (false, false, true, true, true)),
+    ("+ ExprOpt", (false, false, false, true, true)),
+    ("+ Tree", (false, false, false, true, false)),
+    // ("+ Cache (full)", (false, false, false, false, false)),
+];
+
+mod bench;
+use bench::*;
+
+fn run_synthesis(
+    z3_workers: &mut Z3WorkerPool,
+    vrs_file: &str,
+    tag: String,
+    myopts: &(bool, bool, bool, bool, bool),
+) -> Option<BenchResults> {
+    let mut results = BenchResults::new(tag);
+
+    // start of the benchmark
+    let t_start = Instant::now();
+    let t_0 = t_start;
+
+    // construct the AST for the spec
+    let mut ast = match VelosiAst::from_file(vrs_file) {
+        AstResult::Ok(ast) => ast,
+        AstResult::Issues(ast, _e) => ast,
+        AstResult::Err(e) => {
+            println!("   - ERROR: Spec had errors {vrs_file}\n {e}");
+            return None;
+        }
+    };
+    let t_1 = Instant::now();
+
+    // create the models
+    let t_0 = t_1;
+    let models = velosisynth::create_models(&ast);
+    let t_1 = Instant::now();
+
+    let mut num_segments = 0;
+    let mut num_staticmaps = 0;
+    let mut num_enums = 0;
+    let mut total_fields = 0;
+    let mut total_slices = 0;
+    let mut n_queries = 0;
+    let mut n_cached_queries = 0;
+    let mut n_programs = 0;
+    let mut n_programs_max = None;
+
+    let mut map_len = 0;
+    let mut unmap_len = 0;
+    let mut protect_len = 0;
+
+    for unit in ast.units_mut() {
+        if unit.is_abstract() {
+            continue;
+        }
+
+        let mut opts = SynthOpts::new();
+        opts.disable_iface_opt = myopts.0;
+        opts.disable_state_opt = myopts.1;
+        opts.disable_expr_opt = myopts.2;
+        opts.disable_cache_opt = myopts.3;
+        opts.disable_tree_opt = myopts.4;
+        opts.disable_program_generation = true;
+
+        z3_workers.disable_query_cache(opts.disable_cache_opt);
+
+        match unit {
+            VelosiAstUnit::Segment(u) => {
+                num_segments += 1;
+
+                total_fields += u.interface.fields().len();
+                total_slices += u
+                    .interface
+                    .fields()
+                    .iter()
+                    .map(|f| f.layout().len())
+                    .sum::<usize>();
+
+                let t_0 = Instant::now();
+
+                // obtain the mutable reference to the segment
+                let seg: &mut VelosiAstUnitSegment =
+                    Rc::get_mut(u).expect("could not get mut ref!");
+                // create the synthesizer from the factory
+                let mut synth =
+                    Z3SynthSegment::with_opts(seg, models[seg.ident()].clone(), opts, z3_workers);
+
+                let (n_progs, n_progs_max) = synth.num_programs();
+
+                n_programs += n_progs;
+                n_programs_max = if let Some(n) = n_programs_max {
+                    Some(n + n_progs_max.unwrap_or_default())
+                } else {
+                    n_progs_max
+                };
+            }
+            VelosiAstUnit::StaticMap(_s) => {
+                num_staticmaps += 1;
+            }
+            VelosiAstUnit::Enum(e) => {
+                num_enums += 1;
+            }
+            _ => { /* no op */ }
+        }
+    }
+
+    let t_total = Instant::now().duration_since(t_start).as_millis();
+    results.n_cached.push(n_cached_queries.try_into().unwrap());
+    results.n_queries.push(n_queries.try_into().unwrap());
+    results.num_enums = num_enums;
+    results.num_staticmaps = num_staticmaps;
+    results.num_segments = num_segments;
+    results.num_units = num_enums + num_staticmaps + num_segments;
+    results.num_fields = total_fields;
+    results.num_slices = total_slices;
+    results.n_programs = n_programs;
+    results.n_programs_max = n_programs_max;
+
+    Some(results)
+}
+
+fn main() {
+    println!("# Running Benchmark: Synthesis Optimizations");
+
+    let mut latex_results = String::new();
+
+    latex_results.push_str("Spec");
+
+    for (i, (tag, _)) in OPTS.iter().enumerate() {
+        latex_results.push_str(" & ");
+        latex_results.push_str(tag);
+    }
+    latex_results.push_str("\\\\\n");
+    latex_results.push_str("\\hline\n");
+
+    for (spec, name) in SPECS.iter() {
+        println!(" @ Spec: {spec}");
+        let mut row = HashMap::new();
+        for (mytag, opts) in OPTS.iter() {
+            println!("   - Opt: {mytag}");
+
+            let vrs = PathBuf::from(spec);
+            let vrs_file = vrs.display().to_string();
+            if !vrs.is_file() {
+                println!("   - ERROR: Spec not found: {spec}");
+                continue;
+            }
+
+            let tag = format!("{name}{mytag}");
+
+            let mut results = BenchResults::new(tag.clone());
+            let mut had_errors = false;
+
+            // create synth factory and run synthesis on the segments
+            let mut z3_workers = Z3WorkerPool::with_num_workers(NUM_WORKERS, None);
+            if let Some(res) = run_synthesis(&mut z3_workers, vrs_file.as_str(), tag.clone(), opts)
+            {
+                results.merge(&res);
+            } else {
+                had_errors = true;
+                break;
+            }
+
+            z3_workers.terminate();
+
+            row.insert(mytag, results.n_programs_max.unwrap_or_default());
+
+            if had_errors {
+                break;
+            }
+        }
+
+        latex_results.push_str(name);
+        latex_results.push_str(" & ");
+        for (i, (tag, _)) in OPTS.iter().enumerate() {
+            if i > 0 {
+                latex_results.push_str(" & ");
+            }
+            let num = row.get(tag).unwrap().ilog10();
+
+            latex_results.push_str(&format!("{num}"));
+        }
+        latex_results.push_str("\\\\\n")
+    }
+
+    println!("# Completed");
+
+    println!("% latex table\n{latex_results}");
+}
